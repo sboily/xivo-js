4e55a7cfbf28f8bede47d1b4696c01db9728621b
Sylvain Boily <sboily@avencall.com>
2016-01-05 21:09:18 -0500

    fix strip slash

--- src/callcontrol.js
+++ src/callcontrol.js
@@ -279,10 +279,10 @@ XiVOCallControl.prototype.blind_transfer = function(token, originator_call_id, c
         ajax: { headers: { 'X-Auth-Token': token } }
     });
 
-    client.calls.add('transfer');
-    client.calls.transfer.add('blind', {
+    client.calls.add('transfer', {
         stripTrailingSlash: true
     });
+    client.calls.transfer.add('blind');
 
     return client.calls.transfer.blind.create(originator_call_id, call_id, user);
 }
